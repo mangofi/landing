dbf5f26f2c878d1376373987ca335325194e2f19
Hans Gamarra <hans.gamarra1303@gmail.com>
2021-03-13 22:32:33 -0500

    better mango

--- src/components/MangoLogo.js
+++ src/components/MangoLogo.js
@@ -3,7 +3,7 @@ import React from "react"
 function MangoLogo(){
   return (
     <>
-      <img className="mango-logo d-md-none" src="img/mango logo_mobile.svg"></img>
+      <img className="mango-logo bounce d-md-none" src="img/mango.svg"></img>
       <img className="mango-logo mango-logo-sm bounce d-none d-md-block" src="img/mango logo.svg"></img>
     </>
 	)
