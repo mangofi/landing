3e054ba150464863143c39f969bd2935b7c95c36
Hans Gamarra <hans.gamarra1303@gmail.com>
2021-03-26 00:57:48 -0500

    updated mango

--- src/components/MangoLogo.js
+++ src/components/MangoLogo.js
@@ -4,7 +4,7 @@ function MangoLogo(){
   return (
     <>
       <img className="mango-logo d-md-none" src="img/mango.svg"></img>
-      <img className="mango-logo mango-logo-sm bounce d-none d-md-block" src="img/mango logo.svg"></img>
+      <img className="mango-logo mango-logo-sm bounce d-none d-md-block" src="img/mango_logo.svg"></img>
     </>
 	)
 }
